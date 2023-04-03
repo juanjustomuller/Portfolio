5bbf118a146aa381cdf24b48b3bde1a7fc69bf2a
JuanJustoMuller <juancitomuller@gmail.com>
2023-04-03 18:49:33 -0300

    portfolio

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+
+let menu = document.getElementById('menu');
+let toggle_open = document.getElementById('toggle_open');
+let toggle_close = document.getElementById('toggle_close');
+
+toggle_open.addEventListener('click', toggleMenu);
+toggle_close.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    menu.classList.toggle('show-menu');
+
+    if (menu.classList.contains('show-menu')) {
+        toggle_open.style.display = 'none';
+        toggle_close.style.display = 'block';
+    }
+    else {
+        toggle_open.style.display = 'block';
+        toggle_close.style.display = 'none';
+    }
+}
+
+const menuLinks = document.querySelectorAll('.menu a');
+
+menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        const sectionId = menuLink.getAttribute('href');
+        const section = document.querySelector(sectionId);
+        section.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+
